d3b3b84d947605cccc2205a54b9fe2bd024fcb9a
Jorge <jorgelucastcoronado@gmail.com>
2024-05-02 09:49:10 -0300

    feat: test

---
+++ sections/JorgeComponent.tsx
@@ -0,0 +1,15 @@
+export interface Props{
+  /**
+   * @title Post title.
+   */
+  title: string;
+}
+
+export default function JorgeComponent({title}:Props){
+  return (
+    <div>
+      <h1 class="font-bold">{title}</h1>
+      <p>This is an example section</p>
+    </div>
+  )
+}
